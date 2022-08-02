1b713b1f8da48175133c6de1bdc62d13aa08204c
feimingxliu <feimingxliu@gmail.com>
2022-08-02 11:10:23 +0800

    update frontend, remove other analyzers' support.

--- internal/pkg/analyzer/analyzer.go
+++ internal/pkg/analyzer/analyzer.go
@@ -1,8 +1,3 @@
 package analyzer
 
-import (
-	_ "github.com/feimingxliu/quicksearch/internal/pkg/analyzer/gse"
-	_ "github.com/feimingxliu/quicksearch/internal/pkg/analyzer/sego"
-)
-
 // this package just import the underlying analyzer for init()
